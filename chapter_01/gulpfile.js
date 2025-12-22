8725c7810cf5dce157ba6a5f30e862194b06f984
Asanbek09 <samudinua@gmail.com>
2025-12-22 18:01:24 +0600

    Завершение chapter_01

---
+++ chapter_01/gulpfile.js
@@ -0,0 +1,14 @@
+var gulp = require("gulp");
+var shell = require('gulp-shell');
+
+gulp.task("install-binary", shell.task([
+    'go install github.com/narenaryan/romanserver'
+]));
+
+gulp.task("restart-supervisor", ["install-binary"], shell.task(['supervisorctl restart myserver']));
+
+gulp.task('watch', function() {
+    gulp.watch("*", ['install-binary', 'restart-supervisor']);
+});
+
+gulp.task('default', ['watch']);
