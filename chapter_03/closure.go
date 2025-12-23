cc4318a81ee97004d3943f54cabb8bcaa236effc
Asanbek09 <samudinua@gmail.com>
2025-12-23 11:33:00 +0600

    chapter 04 начало

---
+++ chapter_03/closure.go
@@ -0,0 +1,18 @@
+package main
+
+import "fmt"
+
+func main2() {
+	numGenerator := generator()
+	for i := 0; i < 5; i++ {
+		fmt.Print(numGenerator(), "\t")
+	}
+}
+
+func generator() func() int {
+	var i = 0
+	return func() int {
+		i++
+		return i
+	}
+}
