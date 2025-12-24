9422f85795e80d2230edbf491ccac91836540587
Asanbek09 <samudinua@gmail.com>
2025-12-24 14:40:40 +0600

    Завершение chapter_03

---
+++ chapter_03/json_rpc/server.go
@@ -0,0 +1,60 @@
+package main
+
+import (
+	jsonparse "encoding/json"
+
+	"io/ioutil"
+	"log"
+	"net/http"
+	"os"
+
+	"github.com/gorilla/mux"
+	"github.com/gorilla/rpc"
+	"github.com/gorilla/rpc/json"
+)
+
+type Args struct {
+	Id string
+}
+
+type Book struct {
+	Id string `"json:string,omitempty"`
+	Name string `"json:name,omitempty"`
+	Author string `"json:author,omitempty"`
+}
+
+type JSONServer struct {}
+
+func (t *JSONServer) GiveBookDetail(r *http.Request, args *Args, reply *Book) error {
+	var books []Book
+
+	raw, readerr := ioutil.ReadFile("./books.json")
+	if readerr != nil {
+		log.Println("error:", readerr)
+		os.Exit(1)
+	}
+
+	marshalErr := jsonparse.Unmarshal(raw, &books)
+	if marshalErr != nil {
+		log.Println("error:", marshalErr)
+		os.Exit(1)
+	}
+
+	for _, book := range books {
+		if book.Id == args.Id {
+			*reply = book
+			break
+		}
+	}
+
+	return nil
+}
+
+func main() {
+	s := rpc.NewServer()
+	s.RegisterCodec(json.NewCodec(), "application/json")
+	s.RegisterService(new(JSONServer), "")
+	r := mux.NewRouter()
+	r.Handle("/rpc", s)
+	http.ListenAndServe(":1234", r)
+}
